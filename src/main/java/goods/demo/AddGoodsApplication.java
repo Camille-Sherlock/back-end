6b483d730846da6cf33af25879cb82e0204f86d4
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-03 00:54:33 +0800

    [lk]add goods class

---
+++ src/main/java/goods/demo/AddGoodsApplication.java
@@ -0,0 +1,13 @@
+package goods.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddGoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goods/demo/AddGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package goods.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
